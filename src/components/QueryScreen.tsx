38af7831fdc681e8f3220ae836aa8a6095267a71
Ben Collins <sstben@gmail.com>
2024-01-15 19:01:41 -0800

    oajihsd

---
+++ src/components/QueryScreen.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import CrowdSyncLogo from "../images/Crowdsync_Logo.png";
+import styles from "./style";
+
+const QueryScreen = () => {
+  const [user_url, setUserUrl] = useState("");
+  const [event_name, setEventName] = useState("");
+  const navigation = useNavigate();
+
+  const handleSubmit = () => {
+    // Navigate to the SignUp screen
+    navigation("/results");
+  };
+
+  return (
+    <div style={styles.index}>
+      <div style={styles.div}>
+        <div style={{ backgroundColor: "#f0f0f0" }}>
+          <div style={styles.titleContainer}>
+            <img
+              src={CrowdSyncLogo}
+              alt="CrowdSync Logo"
+              style={styles.splashLogo}
+            />
+            <div style={{ marginTop: 20 }} />
+            <span style={styles.splashTitle}>CrowdSync</span>
+          </div>
+          <div style={{ marginTop: 30 }}>
+            <input
+              id="user_url"
+              type="text"
+              placeholder="LinkedIn User URL"
+              value={user_url}
+              onChange={(e) => setUserUrl(e.target.value)}
+              style={styles.textInput}
+            />
+          </div>
+          <div style={{ paddingTop: 20, paddingBottom: 20 }}>
+            <input
+              id="event"
+              type="text"
+              placeholder="Event"
+              value={event_name}
+              onChange={(e) => setEventName(e.target.value)}
+              style={styles.textInput}
+            />
+          </div>
+          <div
+            style={{
+              paddingTop: 10,
+              paddingBottom: 10,
+              paddingLeft: 13,
+              paddingRight: 13,
+            }}
+          >
+            <button onClick={handleSubmit} style={styles.tertiaryButton}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QueryScreen;
---
+++ src/components/ResultsScreen.tsx
@@ -0,0 +1,117 @@
+import React from "react";
+import "react-native-get-random-values";
+
+const ResultsScreen = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#001f3f",
+        color: "#fff",
+        height: "100vh", // Make the background cover the whole screen
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>CrowdSync</h1>
+      <div>
+        <h2>User</h2>
+        <p>Chris Huynh</p>
+      </div>
+      <div>
+        <h3>Event</h3>
+        <p>
+          <a
+            href="https://www.denverstartupweek.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "red",
+              textDecoration: "underline",
+              fontSize: "20px",
+            }}
+          >
+            Denver Startup Week
+          </a>
+        </p>
+      </div>
+      <div>
+        <h3>Possible Matches</h3>
+        <table
+          style={{
+            width: "100%",
+            marginTop: "10px",
+            borderCollapse: "collapse",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Shared Interests</th>
+              <th>Cold Open</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td style={{ padding: "10px" }}>1.</td>
+              <td style={{ padding: "10px" }}>Ben Collins</td>
+              <td style={{ padding: "10px" }}>
+                Entrepreneur, Techstars, Machine Learning
+              </td>
+              <td style={{ padding: "10px" }}>
+                Did you hear about the upcoming Techstars Demo Day? It will have
+                a surprise guest speaker.
+              </td>
+            </tr>
+            <tr>
+              <td style={{ padding: "10px" }}>2.</td>
+              <td style={{ padding: "10px" }}>Malte Witt</td>
+              <td style={{ padding: "10px" }}>
+                Entrepreneur, Denver, Apple Inc.
+              </td>
+              <td style={{ padding: "10px" }}>
+                I saw you recently went to the Utah Startup Week. What was the
+                most standout startup you saw?
+              </td>
+            </tr>
+            <tr>
+              <td style={{ padding: "10px" }}>3.</td>
+              <td style={{ padding: "10px" }}>Ben Deda</td>
+              <td style={{ padding: "10px" }}>
+                Entrepreneur, Techstars, Machine Learning
+              </td>
+              <td style={{ padding: "10px" }}>
+                Ben Deda just launched a new app, crowdsync.com, ask him about
+                it!
+              </td>
+            </tr>
+            <tr>
+              <td style={{ padding: "10px" }}>4.</td>
+              <td style={{ padding: "10px" }}>John Doe</td>
+              <td style={{ padding: "10px" }}>
+                Apple Inc, Accenture, Citadel, Point72
+              </td>
+              <td style={{ padding: "10px" }}>
+                You two used to be coworkers at Accenture. Ask him about his
+                latest ventures.
+              </td>
+            </tr>
+            <tr>
+              <td style={{ padding: "10px" }}>5.</td>
+              <td style={{ padding: "10px" }}>Jane Eyre</td>
+              <td style={{ padding: "10px" }}>
+                Crowdsync, LinkedIn, Sequioa Alumni Day, Harvard University
+              </td>
+              <td style={{ padding: "10px" }}>
+                Jane Eyre just went to CrowdSync's launch party last week!
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+export default ResultsScreen;
